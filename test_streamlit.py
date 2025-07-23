fa1b675d7484a0b294d7d7f650d29e8b8b075cbf
MonicaYou-cloud <tina20001214@gmail.com>
2025-07-23 10:01:54 +0800

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -84,11 +84,13 @@ if st.session_state.page > 0 and st.session_state.start_time:
 
 # 歡迎頁
 if st.session_state.page == 0:
-    st.title("歡迎參加本測驗")
-    st.write("本測驗包含數題圖片與選項，請專心作答。")
-    st.button("開始測驗", on_click=next_page)
-
-
+    st.header("歡迎參加本測驗")
+    st.write("此處將放上實驗說明與知情同意")
+    
+    col1, col2, col3, col4, col5, col6 = st.columns([1, 1, 1, 1, 1, 1])
+        with col6:
+            st.button("開始測驗", on_click=next_page)
+            
 # 基本資料頁
 elif st.session_state.page == 1:
     st.header("基本資料")
@@ -97,13 +99,17 @@ elif st.session_state.page == 1:
     # 問題選項，加上 key 儲存在 session_state
     st.radio("請問您是否為大專院校的學生？", ["是", "否"], index=None, key="age")
     st.radio("請選擇您的性別", ["男", "女", "其他"], index=None, key="gender")
+ 
+col1, col2, col3, col4, col5, col6 = st.columns([1, 1, 1, 1, 1, 1])
+        with col6:
+            st.button("下一頁", on_click=next_page)
 
     if st.button("下一頁"):
         if st.session_state.age is None or st.session_state.gender is None:
             st.warning("請完整填寫所有問題才能繼續。")
         else:
             st.session_state.page += 1
-            st.rerun()  # ✅ 避免需要按兩下的問題
+            st.rerun()   # 避免需要按兩下的問題
             
 # 高級圖形測驗函式
 def graphical_question(
