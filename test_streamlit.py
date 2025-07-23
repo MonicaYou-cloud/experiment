886e501d5d3c6c5636053f789a4918ba48383a95
MonicaYou-cloud <tina20001214@gmail.com>
2025-07-23 17:54:12 +0800

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -158,13 +158,6 @@ def graphical_question(
             st.session_state[f'show_answer_{page_number}'] = False
         if f'show_explanation_{page_number}' not in st.session_state:
             st.session_state[f'show_explanation_{page_number}'] = False
-        
-        # 顯示答案與詳解
-        if st.session_state[f'show_answer_{page_number}']:
-            st.markdown(f"""正確答案是 **{answer_value}**""")
-
-        if st.session_state[f'show_explanation_{page_number}']:
-            st.markdown(f"""詳解：{explanation_text}""")
 
         # 三個按鈕
         col1, col2, col3, col4, col5, col6 = st.columns([1, 1, 1, 1, 1, 1])
@@ -182,6 +175,13 @@ def graphical_question(
             
         with col6:
             st.button("下一頁", on_click=next_page)
+
+        # 顯示答案與詳解
+        if st.session_state[f'show_answer_{page_number}']:
+            st.markdown(f"""正確答案是 **{answer_value}**""")
+
+        if st.session_state[f'show_explanation_{page_number}']:
+            st.markdown(f"""詳解：{explanation_text}""")
 
 # 區分性向測驗函式
 def graphical_question1(
