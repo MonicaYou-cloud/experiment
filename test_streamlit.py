a41162e244c9b40609617d0e372100b0bf2a2525
MonicaYou-cloud <tina20001214@gmail.com>
2025-07-23 19:15:38 +0800

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -440,19 +440,19 @@ elif st.session_state.page == 13:
     with col1:
         E1 = st.radio(label="您覺得自己有多認真對待剛才的練習題？",
                       options=["非常不認真", "不認真", "有點不認真", "有點認真", "認真", "非常認真"],
-                      key=E1,
+                      key="E1",
                       horizontal=True, 
                       index=None
                      )
         E2 = st.radio(label="您覺得自己有多投入於練習階段？",
                       options=["非常不投入", "不投入", "有點不投入", "有點投入", "投入", "非常投入"],
-                      key=E2,
+                      key="E2",
                       horizontal=True, 
                       index=None
                      )
         E3 = st.radio(label="您覺得自己在做練習題時有多努力？",
                       options=["非常不努力", "不努力", "有點努力", "有點努力", "努力", "非常努力"],
-                      key=E3,
+                      key="E3",
                       horizontal=True, 
                       index=None
                      )
@@ -461,14 +461,11 @@ elif st.session_state.page == 13:
             st.warning(st.session_state.warning_message)
 
     with col2:
-        if st.button("下一頁"):
-            if age is None or gender is None:
-                st.session_state.warning_message = "⚠請填寫所有問題才能繼續。"
-                st.rerun()
-            else:
-                st.session_state.warning_message = "" 
-                st.session_state.page += 1
-                st.rerun()
+        if st.session_state.get("E1") is None or \
+        st.session_state.get("E2") is None or \
+        st.session_state.get("E3") is None:
+            st.session_state.warning_message = "⚠請填寫所有問題才能繼續。"
+            st.rerun()
 
 
 # # 完成頁面
