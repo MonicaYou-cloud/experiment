5f5fb3597a2ec7c95579be5875d040f99e7a5fad
MonicaYou-cloud <tina20001214@gmail.com>
2025-08-28 11:53:24 +0800

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -104,8 +104,6 @@ elif st.session_state.page == 1:
     st.markdown("---")
     gender = st.radio("請選擇您的生理性別", ["男", "女", "其他"], horizontal=True, index=None, key="gender")
     age = st.selectbox("請選擇您的年齡區間", ["18歲以下", "19-25歲", "26-35歲", "36-45歲", "46-55歲", "56-65歲", "65歲以上"], index=None, placeholder="請選擇")
-    st.write("您選擇的年齡區間：", age)
-    age = st.radio("請選擇您的年齡區間（歲）", ["18以下", "19-25", "26-35", "26-45", "46-55", "56-65", "65以上",], horizontal=True, index=None, key="age")
     st.write("1. 我一無是處。")
     self_esteem1 = st.radio("（１=非常不同意，６=非常同意）", ["1", "2", "3", "4", "5", "6"], horizontal=True, index=None, key="self_esteem1")
     st.write("2. 我有許多優點。")
@@ -1592,6 +1590,7 @@ elif st.session_state.page == 117:
     st.markdown("""<script>window.scrollTo(0, 0);</script>""", unsafe_allow_html=True)
     st.success("實驗已完成！非常感謝您的參與。")
     st.balloons()
+
 
 
 
