477a632294649693e2ce54659616858066a4a8d6
MonicaYou-cloud <tina20001214@gmail.com>
2025-08-28 09:08:26 +0800

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -157,7 +157,7 @@ elif st.session_state.page == 2:
     with col4:
         if st.button("直接進入正式測驗"):
             st.session_state.start_time = None
-            st.session_state.page = 106
+            st.session_state.page = 105
             st.session_state.scroll_to_top = True
             st.rerun()
     
@@ -211,7 +211,7 @@ def graphical_question(
 
         with col2:
             if st.button("直接進入正式測驗"):
-                st.session_state.page = 106
+                st.session_state.page = 105
                 st.session_state.scroll_to_top = True
                 st.rerun()
     
@@ -1556,6 +1556,7 @@ elif st.session_state.page == 117:
     st.markdown("""<script>window.scrollTo(0, 0);</script>""", unsafe_allow_html=True)
     st.success("實驗已完成！非常感謝您的參與。")
     st.balloons()
+
 
 
 
