f473f18920bd3bcaf05682922b5d5d7c94dc9cc8
MonicaYou-cloud <tina20001214@gmail.com>
2025-08-28 09:28:58 +0800

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -1122,11 +1122,11 @@ if st.session_state.page == 105:
 
     with col1:
         E1 = st.radio(
-            "您覺得自己有多認真對待剛才的練習題？",
+            label="您覺得自己有多認真對待剛才的練習題？（1=非常不認真，6=非常認真）",
             options=["1", "2", "3", "4", "5", "6"],
             key="E1", horizontal=True, index=None
         )
-        st.caption("1 = 非常不認真 6 = 非常認真")
+       
         
         E2 = st.radio(
             label="您覺得自己有多投入於練習階段？",
@@ -1568,6 +1568,7 @@ elif st.session_state.page == 117:
     st.markdown("""<script>window.scrollTo(0, 0);</script>""", unsafe_allow_html=True)
     st.success("實驗已完成！非常感謝您的參與。")
     st.balloons()
+
 
 
 
