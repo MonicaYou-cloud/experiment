16b7e5d79c6ee82c28073bcd533083804c57fdf8
MonicaYou-cloud <tina20001214@gmail.com>
2025-09-12 23:54:25 +0800

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -173,13 +173,28 @@ elif st.session_state.page == 1:
     with col4:
              warning_needed = False
              if st.button("下一頁"):
-                      if (age is None or gender is None or self_esteem1 is None or self_esteem2 is None or self_esteem3 is None 
-                          or self_esteem4 is None or self_esteem5 is None or self_esteem6 is None or self_esteem7 is None 
-                          or self_esteem8 is None or self_esteem9 is None or self_esteem10 is None or mindset1 is None 
-                          or mindset2 is None or mindset3 is None or important1 is None or important2 is None or important3 is None):
-                                   warning_needed = True
+                      if st.session_state.get("ID") is None or
+                      st.session_state.get("gender") is None or \
+                      st.session_state.get("age") is None or \
+                      st.session_state.get("self_esteem1") is None or \
+                      st.session_state.get("self_esteem2") is None or \
+                      st.session_state.get("self_esteem3") is None or \
+                      st.session_state.get("self_esteem4") is None or \
+                      st.session_state.get("self_esteem5") is None or \
+                      st.session_state.get("self_esteem6") is None or \
+                      st.session_state.get("self_esteem7") is None or \
+                      st.session_state.get("self_esteem8") is None or \
+                      st.session_state.get("self_esteem9") is None or \
+                      st.session_state.get("self_esteem10") is None or \
+                      st.session_state.get("mindset1") is None or \
+                      st.session_state.get("mindset2") is None or \
+                      st.session_state.get("mindset3") is None or \
+                      st.session_state.get("important1") is None or \
+                      st.session_state.get("important2") is None or \
+                      st.session_state.get("important3") is None:
+                               warning_needed = True
                       else:
-                               next_page() 
+                               next_page()
                                st.rerun()
     if warning_needed: st.warning("⚠️ 請先作答才能繼續。")
                                                  
@@ -1825,6 +1840,7 @@ elif st.session_state.page == 142:
     st.markdown("""<script>window.scrollTo(0, 0);</script>""", unsafe_allow_html=True)
     st.success("實驗已完成！非常感謝您的參與。")
     st.balloons()
+
 
 
 
