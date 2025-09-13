d1ee966e665e1074573781ecb836fd06278de8e1
MonicaYou-cloud <tina20001214@gmail.com>
2025-09-13 18:13:06 +0800

    Update stage1_1.py

--- stage1_1.py
+++ stage1_1.py
@@ -1917,7 +1917,7 @@ if st.session_state.page == 141:
     st.write("""因此【正式測驗分數並不是真的】，請您別將分數作為判斷自己智力的依據！""")
     st.write("""最後，也請您勿將本研究與測驗內容告知任何人""")
     st.write("""如果您有任何疑問，可以直接聯繫研究人員（112752003@g.nccu.edu.tw）""")
-　　st.write("""祝您能在本研究的抽獎活動中獎！也敬祝平安健康順心！""")
+    st.write("""祝您能在本研究的抽獎活動中獎！也敬祝平安健康順心！""")
     st.markdown("---")
     
     col1, col2, col3, col4 = st.columns([1, 1, 2, 1])
@@ -1930,6 +1930,4 @@ elif st.session_state.page == 142:
     st.markdown("""<script>window.scrollTo(0, 0);</script>""", unsafe_allow_html=True)
     st.success("實驗已完成！非常感謝您的參與。")
     st.balloons()
-
-
 
