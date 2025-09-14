b2b80f641ef2c493621f711e9b942beb721b828d
MonicaYou-cloud <tina20001214@gmail.com>
2025-09-14 15:27:20 +0800

    Update stage1_2.py

--- stage1_2.py
+++ stage1_2.py
@@ -22,7 +22,7 @@ client = gspread.authorize(creds)
 # 📌 這裡只 open 一次
 @st.cache_resource
 def get_sheet():
-    return client.open("experiment_data").worksheet("stage1-1")
+    return client.open("experiment_data").worksheet("stage1-2")
 
 sheet = get_sheet()
 
