713b357cdd7b931488fa6f2d547e333810626723
MonicaYou-cloud <tina20001214@gmail.com>
2025-09-21 20:55:38 +0800

    Update stage1_1.py

--- stage1_1.py
+++ stage1_1.py
@@ -1823,8 +1823,7 @@ if st.session_state.page == 138:
                              st.session_state["mindset1"], st.session_state["mindset2"], st.session_state["mindset3"],
                              st.session_state["important1"], st.session_state["important2"], st.session_state["important3"],
                              st.session_state["Num"], st.session_state["Time"], 
-                             st.session_state["E1"], st.session_state["E2"], st.session_state["E3"],
-                             st.session_state.get("formal_start_time").strftime("%Y-%m-%d %H:%M:%S")
+                             st.session_state["E1"], st.session_state["E2"], st.session_state["E3"], time_str
                      ]
                  sheet.append_row(row_data)
                  next_page() 
@@ -2003,6 +2002,7 @@ elif st.session_state.page == 142:
     st.markdown("""<script>window.scrollTo(0, 0);</script>""", unsafe_allow_html=True)
     st.success("實驗已完成！非常感謝您的參與。")
     st.balloons()
+
 
 
 
