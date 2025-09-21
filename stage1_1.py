f3f18881623afe774faef643c0f455730f978924
MonicaYou-cloud <tina20001214@gmail.com>
2025-09-21 20:44:17 +0800

    Update stage1_1.py

--- stage1_1.py
+++ stage1_1.py
@@ -1781,31 +1781,57 @@ if st.session_state.page == 138:
     st.header("測驗結果")
     st.markdown("---")
 
-    # if st.session_state.get("formal_start_time"):
-    #     elapsed_seconds = int(time.time() - st.session_state.formal_start_time)
-    #     minutes = elapsed_seconds // 60
-    #     seconds = elapsed_seconds % 60
-    #     time_str = f"{minutes} 分 {seconds} 秒"
-    # else:
-    #     time_str = "無法取得"
+    if st.session_state.get("formal_start_time"):
+        elapsed_seconds = int(time.time() - st.session_state.formal_start_time)
+        minutes = elapsed_seconds // 60
+        seconds = elapsed_seconds % 60
+        time_str = f"{minutes} 分 {seconds} 秒"
+    else:
+        time_str = "無法取得"
 
     personal_score = 65
     average_score = 80
+    
+    col1, col2, col3 = st.columns([2, 1, 2]) 
+    with col1: 
+        st.metric(label="您在測驗所花費的時間", value=time_str)  
 
-    col1, col2, col3 = st.columns([2, 1, 2])
-    with col1:
-        st.metric(label="您的智力測驗分數", value=f"{personal_score}")
-    with col3:
-        st.metric(label="與您同齡的人的平均分數", value=f"{average_score}")
+    with col2: 
+        st.metric(label="您的分數", value=f"{personal_score}") 
 
+    with col3: 
+        st.metric(label="與您同齡的人的平均分數", value=f"{average_score}") 
+         
+    # col1, col2, col3 = st.columns([2, 1, 2])
+    # with col1:
+    #     st.metric(label="您的智力測驗分數", value=f"{personal_score}")
+    # with col3:
+    #     st.metric(label="與您同齡的人的平均分數", value=f"{average_score}")
+        
     st.markdown("---")
     # 下一頁按鈕
     col1, col2 = st.columns([5, 2])
     with col2:
         if st.button("下一頁"):
-            st.session_state.page += 1
-            st.session_state.scroll_to_top = True
-            st.rerun()
+                 row_data = [st.session_state["ID"],
+                             st.session_state["ID"], st.session_state["gender"], st.session_state["age"],
+                             st.session_state["self_esteem1"], st.session_state["self_esteem2"],
+                             st.session_state["self_esteem3"], st.session_state["self_esteem4"],
+                             st.session_state["self_esteem5"], st.session_state["self_esteem6"],
+                             st.session_state["self_esteem7"], st.session_state["self_esteem8"],
+                             st.session_state["self_esteem9"], st.session_state["self_esteem10"],
+                             st.session_state["mindset1"], st.session_state["mindset2"], st.session_state["mindset3"],
+                             st.session_state["important1"], st.session_state["important2"], st.session_state["important3"],
+                             st.session_state["Num"], st.session_state["Time"], 
+                             st.session_state["E1"], st.session_state["E2"], st.session_state["E3"],
+                             st.session_state.get("formal_start_time").strftime("%Y-%m-%d %H:%M:%S")
+                     ]
+                     sheet.append_row(row_data)
+                     next_page() 
+                     st.rerun()
+            # st.session_state.page += 1
+            # st.session_state.scroll_to_top = True
+            # st.rerun()
 
 # 操弄檢核
 if st.session_state.page == 139:
@@ -1977,6 +2003,7 @@ elif st.session_state.page == 142:
     st.markdown("""<script>window.scrollTo(0, 0);</script>""", unsafe_allow_html=True)
     st.success("實驗已完成！非常感謝您的參與。")
     st.balloons()
+
 
 
 
