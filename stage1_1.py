12575677075c78f22f2e283cf73c758bf1146ff2
MonicaYou-cloud <tina20001214@gmail.com>
2025-09-22 22:14:09 +0800

    Update stage1_1.py

--- stage1_1.py
+++ stage1_1.py
@@ -1301,7 +1301,8 @@ if st.session_state.page == 105:
                               st.session_state["self_esteem9"], st.session_state["self_esteem10"],
                               st.session_state["mindset1"], st.session_state["mindset2"], st.session_state["mindset3"],
                               st.session_state["important1"], st.session_state["important2"], st.session_state["important3"],
-                              st.session_state["Num"], st.session_state["Time"], st.session_state["E1"], st.session_state["E2"], st.session_state["E3"],
+                              st.session_state["Num"], st.session_state["Num"], st.session_state["Time"], 
+                              st.session_state["E1"], st.session_state["E2"], st.session_state["E3"],
                      ]
                      sheet.append_row(row_data)
                      next_page() 
@@ -2043,6 +2044,7 @@ elif st.session_state.page == 142:
     st.markdown("""<script>window.scrollTo(0, 0);</script>""", unsafe_allow_html=True)
     st.success("實驗已完成！非常感謝您的參與。")
     st.balloons()
+
 
 
 
