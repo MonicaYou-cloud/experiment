a1b3a723f51f6b9fb00204a920424cd0e811a6a1
MonicaYou-cloud <tina20001214@gmail.com>
2025-10-08 20:18:43 +0800

    Update stage1_1.py

--- stage1_1.py
+++ stage1_1.py
@@ -1791,9 +1791,9 @@ if st.session_state.page == 132:
     Img = Image.open("new_folder/圖片1.png")
     col1, col2, col3 = st.columns([1, 2, 1]) 
     with col1:
-             st.metric(label="您先前練習的題數", value=f"{sheet.acell('W34').value} 題")  
+             st.metric(label="您先前練習的題數", value=f"{sheet.acell('T29').value} 題")  
     with col2:
-             st.metric(label="您先前的練習時長", value=sheet.acell("U4").value)  
+             st.metric(label="您先前的練習時長", value=sheet.acell("R4").value)  
     
     col1, col2, col3 = st.columns([1, 1, 2]) 
     with col1:
@@ -1992,5 +1992,6 @@ elif st.session_state.page == 136:
     st.markdown("""<script>window.scrollTo(0, 0);</script>""", unsafe_allow_html=True)
     st.success("實驗已完成！非常感謝您的參與。")
     st.balloons()
+
 
 
