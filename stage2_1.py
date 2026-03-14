57c45d8d379fa44f0b8675676f128a27a915a60a
MonicaYou-cloud <tina20001214@gmail.com>
2026-03-14 22:04:34 +0800

    Update stage2_1.py

--- stage2_1.py
+++ stage2_1.py
@@ -1638,15 +1638,15 @@ if st.session_state.page == 124:
     st.write("""以下問題是想了解您的練習狀況與測驗結果。填寫完畢後請按〔下一頁〕。""")
     st.markdown("---")
      
-    st.write("１. 您比同齡人練習的時間多還是少？")
+    st.write("１. 與同齡人平均的練習時間相比，您的練習時間更多還是更少？")
     prac1 = st.radio(
-             label="（１=您比較多，３=差不多一樣多，５=同齡人們比較多）",
+             label="（１=明顯更多，２=稍微更多，３=差不多一樣，４=稍微更少，５=明顯更少）",
              options=["1", "2", "3", "4", "5"],
              key="prac1", horizontal=True, index=None)
 
-    st.write("２. 您認為誰在練習階段更努力？")
+    st.write("２. 與同齡人相比，您在練習階段付出的努力更多還是更少？")
     prac2 = st.radio(
-             label="（１=您自己更努力，６=同齡人們更努力）",
+             label="（１=更多，６=更少）",
              options=["1", "2", "3", "4", "5", "6"],
              key="prac2", horizontal=True, index=None)
     score1 = st.text_input("３. 您的正式測驗分數是幾分？", placeholder="請輸入數字",  key="score1")
