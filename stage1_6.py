8dbe9dafcfd44e8f54e202d354edec5b40c50161
MonicaYou-cloud <tina20001214@gmail.com>
2026-03-25 12:54:17 +0800

    Update stage1_6.py

--- stage1_6.py
+++ stage1_6.py
@@ -1681,7 +1681,7 @@ if st.session_state.page == 124:
     Img = Image.open("new_folder/圖片2.png")
     col1, col2, col3 = st.columns([1, 2, 1]) 
     with col1:
-             st.metric(label="您先前練習的題數", value=f"{sheet.acell('U22').value} 題")  
+             st.metric(label="您先前練習的題數", value=f"{sheet.acell('U21').value} 題")  
     with col2:
              st.metric(label="您先前的練習時間", value=sheet.acell("S4").value)  
     
